8028ed2dd12d7c6d586be2460ad174e8ecca35c0
Mahaventhan <mahaventhan.s@gmail.com>
2025-01-09 14:03:28 +0530

    Add files via upload
    Assessment program

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+package week1.assignment;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		int a = 101;
+		
+		for (int i = 0; i < a; i++) {
+			
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
